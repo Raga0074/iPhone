23716966e3ec1a68bdef1c5667954e5359cba86e
Harshal Mali <117845618+Raga0074@users.noreply.github.com>
2024-09-05 02:15:34 +0530

    Update Model.jsx

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -6,7 +6,7 @@ import { yellowImg } from "../utils"
 
 
 
-import * as THREE from 'three'
+import * as THREE from "three"
 import { Canvas } from "@react-three/fiber"
 import { View } from "@react-three/drei";
 import { models, sizes } from "../constants"
