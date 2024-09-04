042a77fa8a4115bbdc2e527b3ba82e4ecbba5b02
Harshal Mali <117845618+Raga0074@users.noreply.github.com>
2024-09-05 02:23:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        /^three:.*/,
-      ]
-    }
-  }
+  plugins: [react()]
 })
 
